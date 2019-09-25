8f23a3cca6443775319941ab789cbfb389d96960
Nathan Reuter <nathan0reuter@gmail.com>
2019-09-25 11:57:16 -0300

    adding leads route

--- src/api/contact/controller.js
+++ src/api/contact/controller.js
@@ -1,5 +1,7 @@
 import { sendMail } from '../../services/sendgrid'
 import { contactEmail } from '../../config'
+import UserContact from './model'
+import {success} from '../../services/response';
 
 export const create = ({ bodymen: {body: {email, name, subject, content}} }, res, next) => {
   const contentMessage = `
@@ -15,5 +17,16 @@ export const create = ({ bodymen: {body: {email, name, subject, content}} }, res
     .then(([response]) => response ? res.status(response.statusCode).end() : null)
     .catch(next)
 }
+
+export const index = ({ querymen: { query, select, cursor } }, res, next) =>
+  UserContact.count(query)
+    .then(count => UserContact.find(query, select, cursor)
+      .then(users => ({
+        rows: users.map((user) => user.view()),
+        count
+      }))
+    )
+    .then(success(res))
+    .catch(next)
 
 
--- src/api/contact/index.js
+++ src/api/contact/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { create } from './controller'
+import { create, index } from './controller'
 import { middleware as body } from 'bodymen'
 import { schema } from './model'
 import {emailLeadContact} from '../../middleware/leadsContact'
+import {token} from '../../services/passport';
+import {middleware as query} from "querymen";
 
 const { email, name, subject, content } = schema.tree
 const router = new Router()
@@ -19,5 +21,22 @@ router.post('/contact',
   body({email, name, subject, content}),
   emailLeadContact,
   create)
+
+
+/**
+ * @api {post} /contacts/contact Create contact email
+ * @apiName RetrieveUsersContacts
+ * @apiGroup Contact
+ * @apiPermission admin
+ * @apiParam {String} access_token User access_token.
+ * @apiSuccess {Object[]} users List of usersContacts.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 404 Contact not found.
+ * @apiError 401 Admin access only.
+ */
+router.get('/',
+  token({ required: true, roles: ['admin'] }),
+  query(),
+  index)
 
 export default router
